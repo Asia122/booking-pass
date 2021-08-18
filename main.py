238735824727f869a587d516bbe0626834d18160
Trabubabu <ricky.trabucco@gmail.com>
2021-08-18 14:52:36 +0200

    argument -d to show all dataset

--- main.py
+++ main.py
@@ -12,11 +12,20 @@ parser = argparse.ArgumentParser(description='This program will' +
                                              ' write in a dataset the date of your vaccination ' +
                                              ' check if in a given date you will have the green pass ')
 #la descrizione va fatta meglio
+group = parser.add_mutually_exclusive_group()
+parser.add_argument("name", help="input the fiscal code o a person")
 
-parser.add_argument("name", help="input the name of a known guitarist or band")
+# -d per far uscire tutto il dataset
+group.add_argument("-d", "--database", action="store_true",
+                   help="add a new")
 args = parser.parse_args()
 answer = args.name
 
+if args.database:
+    print("Now you can see by yourself if " +
+          answer + " fiscal code is present in our database manually")
+    print(db)
+    
 print(db["Codice Fiscale"].loc[db["Codice Fiscale"].str.lower() == answer.lower()]
               .values[0], "is the fiscal code of",
               db["Nome"].loc[db["Codice Fiscale"].str.lower() ==
@@ -25,4 +34,6 @@ print(db["Codice Fiscale"].loc[db["Codice Fiscale"].str.lower() == answer.lower(
               answer.lower()].values[0], "whose first dose date is on",
               db["Data prima dose"].loc[db["Codice Fiscale"].str.lower() ==
               answer.lower()].values[0])
+
+
 
