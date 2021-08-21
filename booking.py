f2ad5f976082fa48bc041853b9d6c708bdb9c611
theprove21 <theprove21@users.noreply.github.com>
2021-08-21 13:21:40 +0200

    added booking and a csv file: calendar

---
+++ booking.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def check_avaiability():
+    
+# this function looks if in the column bookings and see if there are some 0 values
+# if there are 0 values means that that place is avaible to book a vaccine
+    
+    df = pd.DataFrame(pd.read_csv('calendar.csv'))
+    lis=df.bookings == 0
+    for i,s in enumerate(lis.values):
+        if s:
+            print('september',df.September[i],'is avaible')
+            # the output is a print for now, but I can easly change it to output the index
+            # of the column 
+            break
