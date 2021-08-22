6cf06fc06952de05cff3be947e9d3b097a19d2f8
Andrea <andrea.rocco@student.h-farm.com>
2021-08-22 16:39:05 +0200

    Created GetSurnameLetters function

--- fiscal_code.py
+++ fiscal_code.py
@@ -1,3 +1,35 @@
-# fiscal_code
+"Function that given name, surname, date of birth, gender, and place of birth is able to calculate the Italian fiscal code."
 
-"Program that from people personal calculates the Italian Fiscal Code"
+def FiscalCodeCalculator(name, surname, dob, gender, pob):
+    fiscalCode = GetSurnameLetters(surname) + GetNameLetters(name) + GetBirthData(dob, gender) + GetPlaceData(pob)
+
+    return fiscalCode + GetControlChar(fiscalCode)
+
+def GetSurnameLetters(surname):
+    surname = surname.upper()
+    result = ""
+    vowels = "AEIOU"
+    consonants = "BCDFGHJKLMNPQRSTVWXYZ"
+    toAnalyze = 0
+    stillConsonants = True
+    stillVowels = True
+
+    while len(result) < 3:
+        if stillConsonants:
+            if surname[toAnalyze] in consonants:
+                result += surname[toAnalyze]
+        elif stillVowels:
+            if surname[toAnalyze] in vowels:
+                result += surname[toAnalyze]
+        else:
+            result += "X"
+
+        if toAnalyze + 1 != len(surname):
+            toAnalyze += 1
+        elif toAnalyze + 1 == len(surname) and stillConsonants:
+            toAnalyze = 0
+            stillConsonants = False
+        else:
+            stillVowels = False
+        
+    return result
