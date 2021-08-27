6fe39def5a5f6cc63f6bd954f62fbb2843390a15
Asia122 <asiamartini122@gmail.com>
2021-08-27 18:59:55 +0200

    Delete Check class from my .py

--- checker_greenpass.py
+++ checker_greenpass.py
@@ -2,22 +2,6 @@ import csv
 import pandas as pd
 from datetime import timedelta, date
 
-class Check:
-
-    def check_fiscalcode(self, fiscalcode):
-        """
-        This function controls if the fiscal code given
-        by the user is present in the fiscal code
-        column inside the people_vaccinated csv file.
-        """
-        fiscalcode = fiscalcode.lower()
-        db = pd.DataFrame(pd.read_csv('people_vaccinated.csv'))
-        fcodes = db["Fiscal Code"].str.lower()
-        if fiscalcode in fcodes.values:
-            return True
-        return False
-
-
 nperson = input()
 def CheckGreenPass(nperson):
     
