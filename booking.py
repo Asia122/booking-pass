61fa49a8c74c48f468592d1a86a6cc2a2ce56bc5
theprove21 <theprove21@users.noreply.github.com>
2021-08-28 16:06:13 +0200

    now allows a maximum of 3 bookings

--- booking.py
+++ booking.py
@@ -1,11 +1,6 @@
-import datetime
 import calendar
+import datetime
 import pandas as pd
-
-y = 2021  # int(input('inserisci anno'))
-m = 8  # int(input('inserisci mese'))
-d = 30  # int(input('inserisci giorno'))
-
 
 def make_appointment(y, m, d):
     booking = datetime.date(y, m, d)
@@ -13,6 +8,7 @@ def make_appointment(y, m, d):
     df = pd.DataFrame(pd.read_csv('cal.csv'))
     now = datetime.date.today()
     lis_reader = []
+    counter=0
 
     if y < now.year:
         print('you cannot select previous years')
@@ -32,9 +28,13 @@ def make_appointment(y, m, d):
             lis_reader.append(time_reader)
 
         for i in lis_reader:
-            if y == i.year and m == i.month and d == i.day:  # checking for already selected date
-                print('you cannot select this date')
-                return 1
+            if y == i.year and m == i.month and d == i.day:
+                counter=counter+1
+        if counter == 3:
+            print('you cannot select this date')
+            return 1# checking for already selected date
+
+
 
         else:
             df2 = pd.DataFrame({'appointment': [str(booking)]})
@@ -43,14 +43,13 @@ def make_appointment(y, m, d):
             df.to_csv('cal.csv', index=False)
             print('you have booked')
 
-
-def avaiability_days(year, month):
+def avaiability_days(y, m):
     df = pd.DataFrame(pd.read_csv('cal.csv'))
     lis_reader = []
     all_current_month_booked_days = []
 
     cal = calendar.Calendar()
-    all_days = cal.monthdayscalendar(year, month)
+    all_days = cal.monthdayscalendar(y, m)
     all_days_clean = []
 
     for row in all_days:
@@ -63,13 +62,14 @@ def avaiability_days(year, month):
         lis_reader.append(time_reader)  # creates a list of datetime object of all the booked date
 
     for i in lis_reader:
-        if i.month == month:
+        if i.month == m:
             all_current_month_booked_days.append(i.day)  # list of all the  month booked days
 
     all_current_month_booked_days.sort()
 
     for booked_day in all_current_month_booked_days:
-        if booked_day in all_days_clean:
+        counter = [i for i, day in enumerate(all_current_month_booked_days) if day == booked_day]  # => [1, 3]
+        if booked_day in all_days_clean and len(counter) >= 3:
             all_days_clean.remove(booked_day)
 
     if all_days_clean == []:
@@ -81,11 +81,13 @@ def avaiability_entire_year(year, month):
     control = avaiability_days(year,
                                month)  ### completare la funzione tenendo in conto la situa di quando tutti i mesi sono booked e quindi cambi anno
 
-    while control == 1:
+    while control == 42:
         month = month + 1
+
         control = avaiability_days(year, month)
 
-        if month == 12 and control == 1:
+        if month == 12 and control == 42:
             return 42
             break
-    return month, avaiability(year, month)
+
+    return month, avaiability_days(year, month)
