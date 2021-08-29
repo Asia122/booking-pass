e81d50894a3c0232cd92800876de17c148b8035d
Asia122 <asiamartini122@gmail.com>
2021-08-29 23:07:59 +0200

    Updatings in checker.py

--- checker.py
+++ checker.py
@@ -1,34 +1,35 @@
+from datetime import date, timedelta, datetime
+import csv
+import pandas as pd
+from checker import Check
+
 """
-The module checker_greenpass.py focuses on checking
-if a oerson has the GreenPass or not.
+The module checker.py focuses on checking
+if a person has the GreenPass or not.
 In order to establish it, the date of the vaccination
 must be checked. If 15 days have passed from that date,
 (including the 15th day) the user has the GreenPass, otherwise not.
 This information is accessible only to the restaurateur.
-In order to do that, the function CheckGreenPass() is used.
-"""
+In order to do that, the function check_green_pass() is used.
 
-"""
 First of all, it has to be checked if the user
 is present in the csv file people_vaccinated.
 If not, it means it doesn't even have the reservation
 for the vaccination.
 """
-import csv
-import datetime
-import pandas as pd 
-from datetime import date, timedelta, datetime
-import pandas as pd
 
 
 class Check:
 
     def check_fiscalcode(self, nperson):
+        # nperson = fiscal code of the user
+
         """
         This function controls if the fiscal code given
         by the user is present in the fiscal code
         column inside the people_vaccinated csv file.
         """
+
         fiscalcode = nperson.lower()
         db = pd.DataFrame(pd.read_csv('people_vaccinated.csv'))
         fcodes = db["Fiscal Code"].str.lower()
@@ -37,9 +38,7 @@ class Check:
         return False
 
 
-    # nperson = fiscal code of the user
-
-    def CheckGreenPass(self, nperson):
+    def check_green_pass(self, nperson):
     
         today = date.today()
         # return the current local date (without the time)
@@ -49,14 +48,16 @@ class Check:
             df = pd.DataFrame(pd.read_csv("people_vaccinated.csv"))
             
             nperson_date = df.loc[df["Fiscal Code"] == nperson,"Date First Shot"]
-            # to select the corresponding date of vaccination given the fiscal code of the user
+            # to select the corresponding date of vaccination given
+            # the fiscal code of the user
             
             nperson_date = nperson_date.to_string()
             nperson_date = nperson_date[-10:]
             nperson_date = datetime.strptime(nperson_date,"%d/%m/%Y").date()
             
             """
-            Since the column df["Date First Shot"] is a pandas.core.series.Series,
+            Since the column df["Date First Shot"] is
+            a pandas.core.series.Series,
             the column's entries have to be transformed in datetime.
             """
             
@@ -75,7 +76,7 @@ class Check:
                 print( nperson + " " + "has the Green Pass.")
     
         else:
-            print("Sorry, but " + nperson + " " + "doesn't even have the reservation for the vaccination.")
-   
-
-
+            result = "Sorry, but " + nperson + " "
+            result = result + "doesn't have the reservation for the vaccination."
+        
+            print(result)
