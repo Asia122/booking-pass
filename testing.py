44dd79f09ea5578ce1c40681a18e04a29bb339f5
NicoleCusinato00 <nicole.cusinato@gmail.com>
2021-08-31 01:25:06 +0200

    Pep8 testing.py

--- testing.py
+++ testing.py
@@ -23,10 +23,10 @@ class Test(unittest.TestCase):
 
     def setUp(self):
         """
-        The setup() function runs when the program starts. 
+        The setup() function runs when the program starts.
         It is used to set the initial environment properties
         """
-        
+
         self.t_fiscalcode = "MRTSAI99B52C957F"
         self.f_fiscalcode = "mrtsai99b52c957f"
         self.v_fiscalcode = ''
@@ -48,7 +48,7 @@ class Test(unittest.TestCase):
         """
         This function is in the module visualize.py
         """
-        
+
         database = pd.DataFrame(pd.read_csv("../booking-pass/people_vaccinated.csv"))
         self.assertTrue(print_info(self.t_fiscalcode, database))
         self.assertFalse(print_info(self.f_fiscalcode, database))
@@ -58,16 +58,19 @@ class Test(unittest.TestCase):
         This function is in the module access_db.py
         """
 
-        self.assertEqual('admin', check_for_username_correct(self.a_username, self.a_password))
-        self.assertEqual('doctor', check_for_username_correct(self.d_username, self.d_password))
-        self.assertEqual('restaurant', check_for_username_correct(self.r_username, self.r_password))
+        self.assertEqual('admin',
+                         check_for_username_correct(self.a_username, self.a_password))
+        self.assertEqual('doctor',
+                         check_for_username_correct(self.d_username, self.d_password))
+        self.assertEqual('restaurant',
+                         check_for_username_correct(self.r_username, self.r_password))
         self.assertEqual(0, check_for_username_correct(self.w_username, self.w_password))
 
     def test_add_element(self):
         """
         This function is in the module adder.py
         """
-        
+
         self.assertTrue(add_element(self.f_fiscalcode))
         self.assertEqual('present', add_element(self.t_fiscalcode))
 
@@ -75,21 +78,22 @@ class Test(unittest.TestCase):
         """
         This function is in the module adder.py
         """
-        
+
         self.assertTrue(add_element(self.v_fiscalcode))
 
     def test_check_green_pass(self):
         """
         This function is in the module checker.py
         """
-        
+
         self.assertEqual('MRTSAI99B52C957F has the Green Pass.',
                          check_green_pass(self.t_fiscalcode))
         self.assertEqual("BNCMRC68E30A058X doesn't have the Green Pass yet.",
                          check_green_pass(self.green_pass_fiscalcode))
         self.assertEqual("Wrong fiscal code format",
                          check_green_pass(self.wrong2_green_pass_fiscalcode))
-        self.assertEqual("Sorry, but CCCMKI00E10L840D doesn't have the reservation for the vaccination.",
+        self.assertEqual("Sorry, but CCCMKI00E10L840D doesn't have the reservation " +
+                         "for the vaccination.",
                          check_green_pass(self.wrong3_green_pass_fiscalcode))
         self.assertEqual("Wrong fiscal code format",
                          check_green_pass(self.wrong1_green_pass_fiscalcode))
